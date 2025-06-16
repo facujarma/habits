de52d19b59784fb77af91864d1703e4f9cbb4a03
facuJarma <facundojarma@gmail.com>
2025-06-16 19:01:45 -0300

    fix: Update meta tags in RootLayout for improved mobile web app experience

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,10 +17,20 @@ export default function RootLayout({ children }) {
     <html lang="en" className="dark">
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#060606" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,viewport-fit=cover,maximum-scale=1,user-scalable=0"
+        />
+        <meta name="display" content="standalone" />
+        <meta name="theme-color" content="#3630ba" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Mi Argentina" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </Head>
       <body
